7fffbb229905ce5a29a8b375fac03cbe18fc85e5
Wuelian <120396134+Wuelian@users.noreply.github.com>
2023-05-11 23:55:33 +0200

    Creado altaCliente

---
+++ Entrega 2/UPMFit22_09/src/main/java/UPM/CITIM22/UPMFit22_09/InterfazUsNoAdmin.java
@@ -0,0 +1,6 @@
+package UPM.CITIM22.UPMFit22_09;
+
+public interface InterfazUsNoAdmin extends InterfazUsuarioGenerico{
+public String getDni();
+public void setDni(String dni);
+}
---
+++ Entrega 2/UPMFit22_09/src/main/java/UPM/CITIM22/UPMFit22_09/Sistema.java
@@ -0,0 +1,13 @@
+package UPM.CITIM22.UPMFit22_09;
+
+public class Sistema {
+	private static ControladorCliente controlleClient = new ControladorCliente();;
+		
+	
+	 public static void main( String[] args ){
+		 
+		 controlleClient.altaCliente();
+	    
+	    
+	    }
+}
