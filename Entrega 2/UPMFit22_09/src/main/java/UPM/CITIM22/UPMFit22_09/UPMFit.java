19f7d7a471c796426c0eeb88fcbc7fa7e8edd640
jorge <jorge@LAPTOP-DCIFT9BD>
2023-05-17 19:37:17 +0200

    Instancias de monitores y sesiones

--- Entrega 2/UPMFit22_09/src/main/java/UPM/CITIM22/UPMFit22_09/UPMFit.java
+++ Entrega 2/UPMFit22_09/src/main/java/UPM/CITIM22/UPMFit22_09/UPMFit.java
@@ -11,8 +11,8 @@ public class UPMFit {
 	public ControladorAdmin controllerAdmin;
 	public ControladorCliente controllerCliente= new ControladorCliente();
 	public ControladorMonitor controllerMonitor;
-	public viewCurso viewCurso;
 	public ControladorCurso controllerCurso;
+	public Curso controllerSesiones;
 
 	public UPMFit(){
 
@@ -39,6 +39,27 @@ public class UPMFit {
 		controllerCliente.getCliente().add(c5);
 		
 //		 controllerCliente.altaCliente("20,hombre,80,258258123,x1564564,asdqwe,pepe.fernandez@alumnos.upm.es,Pepe,PepeFit");
+		
+		//monitores
+		Monitor m1 = new Monitor(895648, "95681283T", "rojo2A", "ramoncin.perez@upm.es", "Ramon", "RamonGym");
+		controllerMonitor.getMonitor().add(m1);
+		Monitor m2 = new Monitor(895648, "70649821H", "azul5D", "pedrito@upm.es", "Pedro", "PedroPilates");
+		controllerMonitor.getMonitor().add(m2);
+		Monitor m3 = new Monitor(895648, "86588104K", "verde3C", "monica.perez@upm.es", "Monica", "MonicaBici");
+		controllerMonitor.getMonitor().add(m3);
+		Monitor m4 = new Monitor(895648, "37498210P", "amarillo6B", "ana_gomez@upm.es", "Ana", "AnaUPM");
+		controllerMonitor.getMonitor().add(m4);
+		
+		TActividad general, bicicleta, baile, gimnasia;
+		//sesiones
+		SesionCerrada s1 = new SesionCerrada(m1, general , 20, "14:00", "15:00");
+		controllerSesiones.getSesiones().add(s1);
+		SesionCerrada s2 = new SesionCerrada(m2, gimnasia , 20, "17:00", "18:00");
+		controllerSesiones.getSesiones().add(s2);
+		SesionCerrada s3 = new SesionCerrada(m1, bicicleta , 40, "15:00", "16:00");
+		controllerSesiones.getSesiones().add(s3);
+		SesionCerrada s4 = new SesionCerrada(m1, baile , 30, "16:00", "17:00");
+		controllerSesiones.getSesiones().add(s4);
 
 	}
 
@@ -72,7 +93,8 @@ public class UPMFit {
 
 	private void start() {
 
-controllerCliente.altaCliente();
+
+	controllerCliente.altaCliente();
 		
 	}
 	 
