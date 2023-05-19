34df6b5e01ef1498aa54d9699ce82ee68a7434cc
jorge <jorge@LAPTOP-DCIFT9BD>
2023-05-19 13:09:48 +0200

    clienteTest

--- Entrega 2/UPMFit22_09/src/test/java/UPM/CITIM22/UPMFit22_09/ClienteTest.java
+++ Entrega 2/UPMFit22_09/src/test/java/UPM/CITIM22/UPMFit22_09/ClienteTest.java
@@ -2,19 +2,143 @@ package UPM.CITIM22.UPMFit22_09;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ClienteTest {
 
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
 	@Test
-	public void test() {
+	public void testCliente() {
 		fail("Not yet implemented");
 	}
-	
-	
-	
-	
-	
-	
+
+	@Test
+	public void testClienteIntStringIntIntStringStringStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetEdad() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPeso() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSexo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTarjeta() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInscribirseACurso() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetInscripciones() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddInscripcionCurso() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveInscripcionCurso() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUsuarioNoAdmin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUsuarioNoAdminStringStringStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDni() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUsuarioGenerico() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUsuarioGenericoStringStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLogin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLogOut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetNombreUsuario() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLogIn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetContrasena() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCorreo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetNombre() {
+		fail("Not yet implemented");
+	}
 
 }
